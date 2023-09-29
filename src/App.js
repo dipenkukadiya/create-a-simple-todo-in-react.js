71200280fa8ad81a495d851b3c4f47c0226e4f3b
dipenkukadiya <dipenkukadiya@gmail.com>
2023-09-29 12:55:29 +0530

    add checbox and refactor the code

--- src/App.js
+++ src/App.js
@@ -1,154 +1,110 @@
-
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
+import TodoInput from "./component/TodoInput";
+import TodoItem from "./component/TodoItem";
 import ListGroup from "react-bootstrap/ListGroup";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			userInput: "",
-			list: [],
-		};
-	}
+    this.state = {
+      userInput: "",
+      list: [],
+    };
+  }
 
+  updateInput(value) {
+    this.setState({
+      userInput: value,
+    });
+  }
 
-	updateInput(value) {
-		this.setState({
-			userInput: value,
-		});
-	}
+  addItem() {
+    if (this.state.userInput !== "") {
+      const userInput = {
+        id: Math.random(),
+        value: this.state.userInput,
+      };
 
-	addItem() {
-		if (this.state.userInput !== "") {
-			const userInput = {
-		
-				id: Math.random(),
+      const list = [...this.state.list];
+      list.push(userInput);
 
-	
-				value: this.state.userInput,
-			};
+      this.setState({
+        list,
+        userInput: "",
+      });
+    }
+  }
 
+  deleteItem(id) {
+    const list = this.state.list.filter((item) => item.id !== id);
+    this.setState({
+      list,
+    });
+  }
 
-			const list = [...this.state.list];
-			list.push(userInput);
+  editItem(id) {
+    const editedTodo = prompt("Edit the todo:");
+    if (editedTodo !== null && editedTodo.trim() !== "") {
+      const updatedList = this.state.list.map((item) => {
+        if (item.id === id) {
+          return { ...item, value: editedTodo };
+        }
+        return item;
+      });
 
+      this.setState({
+        list: updatedList,
+      });
+    }
+  }
 
-			this.setState({
-				list,
-				userInput: "",
-			});
-		}
-	}
+  render() {
+    return (
+      <Container>
+        <Row
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+            fontWeight: "bolder",
+          }}
+        >
+          TODO THINGS
+        </Row>
 
-	deleteItem(key) {
-		const list = [...this.state.list];
-
-		
-		const updateList = list.filter((item) => item.id !== key);
-
-	
-		this.setState({
-			list: updateList,
-		});
-	}
-
-	editItem = (index) => {
-	const todos = [...this.state.list];
-	const editedTodo = prompt('Edit the todo:');
-	if (editedTodo !== null && editedTodo.trim() !== '') {
-		let updatedTodos = [...todos]
-		updatedTodos[index].value= editedTodo
-		this.setState({
-		list: updatedTodos,
-	});
-	}
-	}
-
-	render() {
-		return (
-			<Container>
-				<Row
-					style={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						fontSize: "3rem",
-						fontWeight: "bolder",
-					}}
-				>
-					TODO THINGS
-				</Row>
-
-				<hr />
-				<Row>
-					<Col md={{ span: 5, offset: 4 }}>
-						<InputGroup className="mb-3">
-							<FormControl
-								placeholder="Write to add your things..."
-								size="lg"
-								value={this.state.userInput}
-								onChange={(item) =>
-									this.updateInput(item.target.value)
-								}
-								aria-label="add something"
-								aria-describedby="basic-addon2"
-							/>
-							<InputGroup>
-								<Button
-									variant="dark"
-									className="mt-2"
-									onClick={() => this.addItem()}
-								>
-									ADD
-								</Button>
-							</InputGroup>
-						</InputGroup>
-					</Col>
-				</Row>
-				<Row>
-					<Col md={{ span: 5, offset: 4 }}>
-						<ListGroup>
-							
-							{this.state.list.map((item, index) => {
-								return (
-								<div key = {index} >
-									<ListGroup.Item
-										variant="dark"
-										action
-										style={{display:"flex",
-												justifyContent:'space-between'
-									}}
-									>
-										{item.value}
-										<span>
-										<Button style={{marginRight:"10px"}}
-										variant = "light"
-										onClick={() => this.deleteItem(item.id)}>
-										Delete
-										</Button>
-										<Button variant = "light"
-										onClick={() => this.editItem(index)}>
-										Edit
-										</Button>
-										</span>
-									</ListGroup.Item>
-								</div>
-								);
-							})}
-						</ListGroup>
-					</Col>
-				</Row>
-			</Container>
-		);
-	}
+        <hr />
+        <Row>
+          <Col md={{ span: 5, offset: 4 }}>
+            <TodoInput
+              value={this.state.userInput}
+              onChange={this.updateInput.bind(this)}
+              onAdd={this.addItem.bind(this)}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ span: 5, offset: 4 }}>
+          <ListGroup>
+              {this.state.list.map((item) => (
+                <TodoItem
+                  key={item.id}
+                  item={item}
+                  onDelete={this.deleteItem.bind(this)}
+                  onEdit={this.editItem.bind(this)}
+                />
+              ))}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/TodoInput.js
@@ -0,0 +1,31 @@
+// TodoInput.js
+import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
+const TodoInput = ({ value, onChange, onAdd }) => {
+  return (
+    <InputGroup className="mb-3">
+      <FormControl
+        placeholder="add item . . . "
+        size="lg"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        aria-label="add something"
+        aria-describedby="basic-addon2"
+      />
+      <InputGroup>
+        <Button
+          variant="dark"
+          className="mt-2"
+          onClick={() => onAdd()}
+        >
+          ADD
+        </Button>
+      </InputGroup>
+    </InputGroup>
+  );
+};
+
+export default TodoInput;
